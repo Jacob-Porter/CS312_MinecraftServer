63ac6a470454e4e3e7fde89ad82c6191fe65f7c2
Jacob-Porter <jkp98199@gmail.com>
2025-06-03 12:39:40 -0700

    README tutorial, main.tf, minecraft-setup.sh

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # CS312_MinecraftServer
+
+## Requirements
+1) [Terraform](https://developer.hashicorp.com/terraform/install) is installed
+2) [AWS CLI](https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html) is installed
+3) Already have or able to create an [ssh key pair](https://www.ssh.com/academy/ssh/keygen)
+
+
+## Configure AWS CLI
+*If you are using AWS Learner Lab environment, ignore this section.*
+1) Identify your AWS Access Key ID and AWS Secret Access Key (if applicable, may require your [AWS session token](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html) as well) [help](https://docs.aws.amazon.com/IAM/latest/UserGuide/id_credentials_access-keys.html)
+2) In your Terminal, run ```aws configure```
+3) Enter information asked and set your preferred region (I'm using us-west-2) *for output format, you can either leave it blank or use 'json'*
+
+## Minecraft Server File
+*this repo provides a minecraft server file already - will require minor, manual changes to work*
+1) Create and name your Minecraft Server .sh file (if using provided file, find "Download mincraft_server._version_.jar" [here](https://www.minecraft.net/en-us/download/server) and paste that link next to ```MINECRAFTSERVERURL=``` in _minecraft-setup.sh_)
+2) Inorder for the EC2 instance to grab the file, you must upload the script to S3. In your terminal (AWS Learner Lab terminal or AWS CLI after [signing in to your AWS account](https://docs.aws.amazon.com/signin/latest/userguide/command-line-sign-in.html)) run ```aws s3 cp minecraft-setup.sh s3://<recognizable name>/scripts/<minecraft server filename>.sh```
+
+## Deploy using Terraform
+*this repo provides a ```main.tf``` already; however, you may need to make changes to it based on server hardware requirements and/or region preferences*
+1) In your terminal, navigate to the location of ```main.tf```
+2) Ensure your key pair path is correctly listed throughout the file (```~/.ssh/id_rsa```)
+3) Edit the line ```"aws s3 cp s3://cs312-minecraft-server/scripts/minecraft-setup.sh /tmp/minecraft-setup.sh``` under ```resource "null_resource" "run_minecraft_setup" {``` so that it looks like ```"aws s3 cp s3://<your s3 bucket name>/scripts/minecraft-setup.sh /tmp/minecraft-setup.sh```
+4) Run the command ```terraform init```
+5) Run the command ```terraform apply```
+6) This should create and setup your EC2 instance with a VPC, subnet, security group, gateway, and Elastic IP, pull your minecraft server file from your S3 bucket, and start your minecraft server file automatically *(it may take a couple of minutes for the minecraft server file to complete)*.
+7) You should see output listing the IP of the EC2. This can be used to connect to in Minecraft itself (or scan using programs such as ```nmap``` at port 25565)
+
+### Sources
+- [Minecraft Server File and EC2 setup](https://aws.amazon.com/blogs/gametech/setting-up-a-minecraft-java-server-on-amazon-ec2/)
+- [Creating and Uploading to AWS S3 bucket via AWS CLI](https://www.geeksforgeeks.org/how-to-upload-files-to-aws-s3-using-cli/)
+- [Terraform tutorials for AWS](https://developer.hashicorp.com/terraform/tutorials/aws-get-started)
+- [AWS networking with Terraform](https://www.geeksforgeeks.org/automating-aws-network-firewall-configurations-with-terraform/)
---
+++ main.tf
@@ -0,0 +1,157 @@
+# provider setup
+provider "aws" {
+  region = "us-west-2" # Change to your preferred region
+}
+
+# SSH key pair (UPDATE PATH FOR CORRECT PK-PAIR: ~/.ssh/id_rsa.pub)
+resource "aws_key_pair" "minecraft_key" {
+  key_name   = "minecraft-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# VPC
+resource "aws_vpc" "minecraft_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "minecraft-vpc"
+  }
+}
+
+# Subnet, update if region changed...
+resource "aws_subnet" "minecraft_subnet" {
+  vpc_id                  = aws_vpc.minecraft_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
+  availability_zone       = "us-west-2a"
+
+  tags = {
+    Name = "minecraft-subnet"
+  }
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "minecraft_igw" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  tags = {
+    Name = "minecraft-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "minecraft_rt" {
+  vpc_id = aws_vpc.minecraft_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.minecraft_igw.id
+  }
+
+  tags = {
+    Name = "minecraft-rt"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "minecraft_rta" {
+  subnet_id      = aws_subnet.minecraft_subnet.id
+  route_table_id = aws_route_table.minecraft_rt.id
+}
+
+# Security Group
+resource "aws_security_group" "minecraft_sg" {
+  name        = "minecraft-server-sg"
+  description = "Allow inbound SSH and Minecraft"
+  vpc_id      = aws_vpc.minecraft_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Minecraft port"
+    from_port   = 25565
+    to_port     = 25565
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "minecraft-server-sg"
+  }
+}
+
+# Elastic IP
+resource "aws_eip" "minecraft_eip" {
+  domain = "vpc"
+}
+
+# Network Interface with EIP
+resource "aws_network_interface" "minecraft_eni" {
+  subnet_id       = aws_subnet.minecraft_subnet.id
+  private_ips     = ["10.0.1.100"]
+  security_groups = [aws_security_group.minecraft_sg.id]
+}
+
+resource "aws_eip_association" "minecraft_eip_assoc" {
+  allocation_id        = aws_eip.minecraft_eip.id
+  network_interface_id = aws_network_interface.minecraft_eni.id
+}
+
+# EC2 Instance, update based on your server requirements/region...
+resource "aws_instance" "minecraft_server" {
+  ami                         = "ami-0b65bee2e046aec19" # Amazon Linux 2023 ARM64 in us-west-2
+  instance_type               = "t4g.small"
+  key_name                    = aws_key_pair.minecraft_key.key_name
+  iam_instance_profile        = "LabInstanceProfile"
+
+  network_interface {
+    device_index         = 0
+    network_interface_id = aws_network_interface.minecraft_eni.id
+  }
+
+  tags = {
+    Name = "MinecraftServer"
+  }
+}
+
+# Use remote-exec and SSM-enabled EC2, update PK path if required...
+resource "null_resource" "run_minecraft_setup" {
+  provisioner "remote-exec" {
+    inline = [
+      "aws s3 cp s3://cs312-minecraft-server/scripts/minecraft-setup.sh /tmp/minecraft-setup.sh",
+      "chmod +x /tmp/minecraft-setup.sh",
+      "sudo /tmp/minecraft-setup.sh"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = aws_eip.minecraft_eip.public_ip
+    }
+  }
+
+  depends_on = [aws_instance.minecraft_server]
+}
+
+# Print server IP
+output "minecraft_server_ip" {
+  value       = aws_eip.minecraft_eip.public_ip
+  description = "Public IP to connect to your Minecraft server"
+}
+
